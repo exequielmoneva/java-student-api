a4912e396e6f00637544b3b322dd846b5b31c625
exequielmoneva <exequielmoneva@gmail.com>
2022-06-24 20:59:49 -0300

    PostgreSQL connection, Get endpoint and refactor

--- src/main/java/com/example/javastudentapi/JavaStudentApiApplication.java
+++ src/main/java/com/example/javastudentapi/JavaStudentApiApplication.java
@@ -2,22 +2,15 @@ package com.example.javastudentapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
-@RestController
+
 public class JavaStudentApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaStudentApiApplication.class, args);
 	}
 
-	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello", "World");
-	}
+
 
 }
