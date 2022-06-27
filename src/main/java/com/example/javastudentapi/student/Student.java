94b41018ba8fd5c890acf4ab27ab7a6c1f1fe144
exequielmoneva <exequielmoneva@gmail.com>
2022-06-27 10:28:41 -0300

    POST endpoint working, minor refactor

--- src/main/java/com/example/javastudentapi/student/Student.java
+++ src/main/java/com/example/javastudentapi/student/Student.java
@@ -2,6 +2,8 @@ package com.example.javastudentapi.student;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
+
 @Entity
 @Table
 public class Student {
@@ -19,6 +21,7 @@ public class Student {
     private Long id;
     private String name;
     private String email;
+    @Transient
     private Integer age;
     private LocalDate dob; // dob = date of birth
 
@@ -26,18 +29,16 @@ public class Student {
     public Student() {
     }
 
-    public Student(Long id, String name, String email, Integer age, LocalDate dob) {
+    public Student(Long id, String name, String email, LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.age = age;
         this.dob = dob;
     }
 
-    public Student(String name, String email, Integer age, LocalDate dob) {
+    public Student(String name, String email, LocalDate dob) {
         this.name = name;
         this.email = email;
-        this.age = age;
         this.dob = dob;
     }
 
@@ -66,7 +67,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
--- src/main/java/com/example/javastudentapi/student/StudentRepository.java
+++ src/main/java/com/example/javastudentapi/student/StudentRepository.java
@@ -3,7 +3,13 @@ package com.example.javastudentapi.student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    // Constructor
+
+    Optional<Student> findStudentByEmail(String email);
 
 }
